39f310097f88cdd1d48b23803c54508e559b0939
Nguyễn Tiến Đạt <122581048+tiedat04@users.noreply.github.com>
2024-05-19 21:31:46 +0700

    upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		<html xmlns="http://www.w3.org/1999/xhtml" lang='vi'>
+		<head>
+		<meta http-equiv="Content-Type" content="application/json; charset=utf-8" />
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'><title>Mẫu website đẹp - Hơn 300+ template web đẹp đa ngành nghề - Halink Web</title>
+		<meta name='robots' content='index,follow' />
+		<meta property=''og:locale' content='vi_VN' />
+		<meta property='og:type' content='website' />
+		<meta property='og:site_name' content='Halink Web' />
+		<meta property='og:description' content="Mẫu website đẹp, template web bán hàng, demo web doanh nghiệp. Thiết kế website trọn gói giá rẻ, chuyên nghiệp, chuẩn SEO, Tặng tên miền và hosting không giới hạn." />
+		<meta property='og:title' content="Mẫu website đẹp - Hơn 300+ template web đẹp đa ngành nghề - Halink Web" />
+		<meta name='og:url' content="https://chonweb.vn" />
+		<meta name='og:image' content="https://chonweb.vn/logo.png" />
+		<meta property='og:image:alt' content='Halink Web' />
+		<meta name='description' content="Mẫu website đẹp, template web bán hàng, demo web doanh nghiệp. Thiết kế website trọn gói giá rẻ, chuyên nghiệp, chuẩn SEO, Tặng tên miền và hosting không giới hạn." />
+		<link rel='image_src' href="https://chonweb.vn/logo.png" />
+		<link rel='icon' href='/favicon.ico' type='image/x-icon' />
+		<meta property='og:image' content="https://chonweb.vn/logo.png" />
+		<link rel='canonical' href="https://chonweb.vn/at10/demo" /><meta name='author' content="Developed by Halink Web - HALINK CO.,LTD - 18006319"/><meta name='generator' content="Developed by Halink Web  - HALINK CO.,LTD - 18006319"/><script src="/hsc_plugin/plugin_sys/sanpham/js/js_xuly_chuoi.js"></script>
+</head><link href="/hsc_skin/chonweb/css/main.css" rel='stylesheet' type='text/css' /><script src="/hsc_plugin/plugin_sys/sanpham/js/js_xuly_chuoi.js"></script>	<link rel="stylesheet" type="text/css" href="/hsc_skin/chonweb/css/fonts/font-awesome.min.css">
+	<script src="/hsc_skin/chonweb/java/jquery_demo.min.js"></script>
+	<script type="text/javascript" src="/hsc_skin/chonweb/java/custom_demo.js"></script>
+</head>
+<iframe src="https://at10.mediawz.com/" style="width: 100%;height: 100vh;"></iframe>
+</div>
+</div>
+</div>
+</div>
